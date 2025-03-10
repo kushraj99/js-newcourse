d9190fa6a982a1ce8643396a4b713265d90f1e37
kushraj99 <711chowdhurykoushik@gmail.com>
2025-03-10 12:45:09 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "kushraj@gmail.com"
+var accountPassword = "23456"
+accountCity = "medinipur"
+
+//accountId = 5 // not allowed
+accountEmail = "kc@google.com"
+accountPassword = "97650"
+accountCity = "kharagpur"// not proper way//
+let accountState
+
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState])
