03796db4643403fc14511d2fe8c1c34bd49f6024
kushraj99 <711chowdhurykoushik@gmail.com>
2025-03-10 12:45:34 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";// treat all js code as newer version
+
+//alert(3 + 3); // we are using nodejs, not browser
+let name = "koushik"
+let age = 25
+let isLoggedIn = false
+
+//number = 2 to the power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null =>stand alone value
+//undefined =>
+// symobl =>for unique
+
+
+//object
+
+console.log(typeof null)// output- object cause null is a object
+console.log(typeof undefined)// output - undefined
+console.log(typeof "symbol")
