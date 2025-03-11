48e453cbcc3d7ece97fbc96d4bf366ffd56ca0ff
kushraj99 <711chowdhurykoushik@gmail.com>
2025-03-11 13:32:43 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = true
+//console.log(typeof score)
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+//"33" = 33
+//"57abc" = not a number
+//true = 1 and false = 0
+
+let isLoggedin = ""
+let booleanIsLoggedIn = Boolean(isLoggedin)
+//console.log(booleanIsLoggedIn)
+//console.log(typeof booleanIsLoggedIn)
+
+//1 = true ; 0 = false
+//"koushik" = true
+//"" = false
+
+let someString = 56
+
+let anotherString = String(someString)
+console.log(anotherString)
+console.log(typeof anotherString)
