ac4cceeb4d1f8ea336f575060f17fedc08bf3963
kushraj99 <711chowdhurykoushik@gmail.com>
2025-03-13 12:46:19 +0000

    number_&_math

---
+++ 01_basics/06_num_&_math.js
@@ -0,0 +1,43 @@
+const score = 100
+//console.log(score);
+
+const balance = new Number(400)
+// console.log(balance)
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));// to get a value upto 2 decimals , output = 400.00
+// const anotherNumber = 1234.567
+// console.log(anotherNumber.toPrecision(4));// to get a precise number , output = 1235
+
+const otherNumber = 10000000000
+//console.log(otherNumber.toLocaleString('en-IN'));// to give a string of numbers in Indian standards , output = 10,00,00,00,000
+
+//+++++++++++++++++++++++++++++++++++++++  MATHS  ++++++++++++++++++++++++++//
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.min(2 , 3 , 4 , 6));
+// console.log(Math.max(2,3,4,5));
+
+// console.log(Math.random());
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min) // to get a random no. from a max and min range
+
+
+
+
+
+
+
+
+
+
+
+
+
+
