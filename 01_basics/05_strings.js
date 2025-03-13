6f803325de7ad84fd3293bb865f5a4d3d6e694e3
kushraj99 <711chowdhurykoushik@gmail.com>
2025-03-13 11:52:40 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,50 @@
+const name = "koushik" // the way to define a string
+const bodyCount = 20
+
+//console.log( name + bodyCount + " value");// this is not a good way to write
+//  console.log(`My name is ${name} and my bodycount is ${bodyCount}`); // proper way
+
+const gameName = new String(`koushikch`)
+// console.log(gameName.length);
+//  console.log(gameName.__proto__);
+ console.log(gameName.charAt(2));
+//  console.log(gameName.indexOf(`s`));
+//  console.log(gameName.toUpperCase());
+
+const newString = gameName.substring(0 , 5) 
+// console.log(newString);
+
+const anotherString = gameName.slice(-8 , 5)
+// console.log(anotherString);
+
+const otherString =  "   koushik   "
+// console.log(otherString);
+//  console.log(otherString.trim());
+
+ const url = "http://kushraj.com/raju chowdhury"
+//   console.log(url.replace(` `, `_`));
+//  console.log(url.includes(`raju`));
+
+const str = "kosuhik is a bad boy!"
+//console.log(str.split(" "));
+//console.log(str.charCodeAt(2));
+console.log(str.lastIndexOf("bad"))
+
+const str1 = "jhuma";
+const str2 = "koushik";
+
+//console.log(str1.concat(" loves " , str2));
+
+const sentence = "I think Ruth's dog is cuter than your dog!"
+//console.log(sentence.replaceAll(`dog` , `pet`));
+
+
+
+
+
+
+ 
+ 
+
+
+
